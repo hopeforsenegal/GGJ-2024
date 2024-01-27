4ebe9a8e8440fb9a73c33d7e7590ebaf458d0bcc
Kamau Vassall <kamau.vassall@gmail.com>
2024-01-27 14:08:27 -0600

    Make the main menu kinda sorta look like something

--- Get to Work/Assets/Scripts/MainMenu.cs
+++ Get to Work/Assets/Scripts/MainMenu.cs
@@ -8,6 +8,8 @@ public class MainMenu : MonoBehaviour
     public Config config;
     public Button play;
     public Button quit;
+    public Image background;
+    public Image title;
 
     protected void Start()
     {
@@ -23,6 +25,11 @@ public class MainMenu : MonoBehaviour
         {
             QuitGame();
         });
+        LeanTween.scale(title.gameObject, Vector3.one * 1.5f, 5).setLoopPingPong();
+        LeanTween.rotate(title.gameObject, Vector3.one * 90, 5).setLoopPingPong();
+
+
+        LeanTween.scale(background.gameObject, Vector3.one * 1.5f, 7).setEaseInElastic().setLoopPingPong();
     }
 
     protected void Update()
@@ -38,7 +45,7 @@ public class MainMenu : MonoBehaviour
 #if UNITY_EDITOR
         EditorApplication.isPlaying = false;
 #else
-            Application.Quit();
+        Application.Quit();
 #endif
     }
 }
