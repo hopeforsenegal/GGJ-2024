1911a8ed05125033327ebf731ad702b18e68b2cb
Kamau Vassall <kamau.vassall@gmail.com>
2024-01-27 00:11:46 -0600

    Got the main menu situated conceptually

--- Get to Work/Assets/Scripts/Config.cs
+++ Get to Work/Assets/Scripts/Config.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Config : ScriptableObject
 {
-    [Header("Ski player")]
+    [Header("Audio")]
+    public AudioClip Menu;  
+
+    [Header("Player")]
+    public bool temp;
+
+    [Header("Player/Ski")]
     public float skiPlayerSpeed;
 
 
-    [Header("Jump player")]
+    [Header("Player/Jump")]
     public float jumpPlayerSpeed;
 
 
-    [Header("Shooter player")]
+    [Header("Player/Shooter")]
     public float shooterPlayerSpeed;
+
+    [Header("Levels")]
+    public string firstLevel;
 }
---
+++ Get to Work/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Config config;
+    public Button play;
+    public Button quit;
+
+    protected void Start()
+    {
+        var a = FindObjectOfType<AudioSource>();
+        a.clip = config.Menu;
+        a.Play();
+
+        play.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(config.firstLevel);
+        });
+        quit.onClick.AddListener(() =>
+        {
+            QuitGame();
+        });
+    }
+
+    protected void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            QuitGame();
+        }
+    }
+
+
+    private static void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
