f3ce019c87ef5d3b850bd4605b32b593dae6ba2c
Kamau Vassall <kamau.vassall@gmail.com>
2024-01-26 21:57:15 -0600

    Creating the config file

---
+++ Get to Work/Assets/Scripts/Config.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Config : ScriptableObject
+{
+    // Ski Player
+    public float skiPlayerSpeed;
+
+    // Jump Player
+
+
+    // Shooter Player
+
+
+
+}
