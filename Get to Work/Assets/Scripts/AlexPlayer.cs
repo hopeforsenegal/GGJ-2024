52192a450e09e7613428b038758d7b7ca95ecc6b
nandbolt <48170421+nandbolt@users.noreply.github.com>
2024-01-27 12:41:29 -0600

    left right movement + stop
    - horizontal movement with Input.GetAxis("Horizontal")
    - stop going down with Input.GetKey("space")

--- Get to Work/Assets/Scripts/AlexPlayer.cs
+++ Get to Work/Assets/Scripts/AlexPlayer.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class AlexPlayer : MonoBehaviour
 {
+    // Movement
+    float xRunSpeed = 0.01f;
+    float yRunSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,19 @@ public class AlexPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(transform.position.x, transform.position.y - 0.01f);
+        // Init input direction
+        float dx = Input.GetAxis("Horizontal");
+        bool run = !Input.GetKey("space");
+
+        // Update velocity
+        float vx = dx * xRunSpeed;
+        float vy = 0.0f;
+        if (run)
+        {
+            vy = -yRunSpeed;
+        }
+
+        // Update position
+        transform.position = transform.position + new Vector3(vx,vy,0.0f);
     }
 }
