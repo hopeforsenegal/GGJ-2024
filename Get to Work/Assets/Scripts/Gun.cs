2fdacb981680ef7033e6286f5d6d33804bd90efd
mtzrecker <javinmac123@gmail.com>
2024-01-27 14:34:06 -0500

    Left and Right Directional Facing

---
+++ Get to Work/Assets/Scripts/Gun.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawn;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawn.right * bulletSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawn.up * bulletSpeed;
+        }
+    }
+}
--- Get to Work/Assets/Scripts/TopDownDude.cs
+++ Get to Work/Assets/Scripts/TopDownDude.cs
@@ -4,21 +4,57 @@ using UnityEngine;
 
 public class TopDownDude : MonoBehaviour
 {
-    public float movSpeed;
+    [HideInInspector]
+    public bool isFacingLeft;
+    public Config config;
     float speedX, speedY;
     Rigidbody2D rb;
+
+    private SpriteRenderer spriteRenderer;
+    public bool test;
+    public bool spawnFacingLeft;
+    private Vector2 facingLeft;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Debug.Log("hello");
+        //Debug.Log("hello");
+        /*facingLeft = new Vector2(-transform.localScale.x, transform.localScale.y);
+        if(spawnFacingLeft)
+        {
+            transform.localScale = facingLeft;
+            isFacingLeft = true;
+        }*/
+        test = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
-        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        speedX = Input.GetAxisRaw("Horizontal") * config.shooterPlayerSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * config.shooterPlayerSpeed;
         rb.velocity = new Vector2(speedX, speedY);
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        /*if (Input.GetKeyDown(KeyCode.LeftArrow)|| (Input.GetKeyDown(KeyCode.D))
+            {
+            test = true;
+            }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || (Input.GetKeyDown(KeyCode.A))
+            {
+            test = false;
+            }
+        if (test = false)
+        {
+            
+        }*/
     }
+    private void FixedUpdate()
+    {
+        spriteRenderer.flipX = rb.velocity.x < 0f;
+
+    }
+
+
 }
