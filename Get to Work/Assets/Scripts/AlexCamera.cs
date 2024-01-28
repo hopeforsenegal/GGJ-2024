e02c6a9fab6d9e373f4a1c87f9252e3d4d5f466a
nandbolt <48170421+nandbolt@users.noreply.github.com>
2024-01-28 05:58:39 -0600

    difficulty scaling

--- Get to Work/Assets/Scripts/AlexCamera.cs
+++ Get to Work/Assets/Scripts/AlexCamera.cs
@@ -7,14 +7,23 @@ public class AlexCamera : MonoBehaviour
     // Goals
     bool levelComplete = false;
     float goalDistance = 500.0f;
+    float threshold1 = 100.0f;
+    float threshold2 = 200.0f;
+    float threshold3 = 300.0f;
+    float threshold4 = 400.0f;
 
     // Spawning
     public GameObject[] obstacles;
+    public GameObject[] actors;
     int spawnFreq = 4;  // low number = high spawn frequency, high number = low spawn frequency
     int spawnRadius = 30;
     int minSpawnCount = 1;
-    int maxSpawnCount = 10;
+    int maxSpawnCount = 4;
     bool notSpawned = true;
+    int actorSpawnTimer = 0;
+    int actorSpawnFreq = 300;
+    int minActorSpawnCount = 1;
+    int maxActorSpawnCount = 1;
 
     // Called once on level complete
     void onLevelComplete()
@@ -31,7 +40,35 @@ public class AlexCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // If at spawn line
+        // Spawn thresholds
+        if (transform.position.y < -threshold4)
+        {
+            minActorSpawnCount = 10;
+            maxActorSpawnCount = 10;
+            minSpawnCount = 2;
+            maxSpawnCount = 10;
+        }
+        else if (transform.position.y < -threshold3)
+        {
+            minActorSpawnCount = 2;
+            maxActorSpawnCount = 6;
+            minSpawnCount = 2;
+            maxSpawnCount = 8;
+        }
+        else if (transform.position.y < -threshold2)
+        {
+            minActorSpawnCount = 1;
+            maxActorSpawnCount = 5;
+            minSpawnCount = 2;
+            maxSpawnCount = 6;
+        }
+        else if (transform.position.y < -threshold1)
+        {
+            minActorSpawnCount = 1;
+            maxActorSpawnCount = 6;
+        }
+
+        // If at spawn line (obstacles)
         if (!levelComplete && (int)transform.position.y % spawnFreq == 0)
         {
             // If not already spawned there
@@ -51,6 +88,21 @@ public class AlexCamera : MonoBehaviour
         else
         {
             notSpawned = true;
+        }
+
+        // If time to spawn an actor
+        actorSpawnTimer += 1;
+        if (!levelComplete && (int)actorSpawnTimer % actorSpawnFreq == 0)
+        {
+            Debug.Log("Spawn!");
+            int spawnCount = Random.Range(minActorSpawnCount, maxActorSpawnCount);
+            for (int i = 0; i < spawnCount; i++)
+            {
+                float x = transform.position.x + Random.Range(-spawnRadius, spawnRadius);
+                float y = transform.position.y - 10f;
+                int actorIdx = Random.Range(0, actors.Length);
+                Instantiate(actors[actorIdx], new Vector3(x, y, 0), Quaternion.identity);
+            }
         }
 
         // Check win condition
