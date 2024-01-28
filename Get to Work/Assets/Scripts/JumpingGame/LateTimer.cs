9e174532fde261a3dbd776c66b654d9a3fe1abef
Kamau Vassall <kamau.vassall@gmail.com>
2024-01-28 16:07:19 -0600

    Removing stuff and making things clear

--- Get to Work/Assets/Scripts/JumpingGame/LateTimer.cs
+++ Get to Work/Assets/Scripts/JumpingGame/LateTimer.cs
@@ -11,6 +11,7 @@ namespace JumpingGame
         {
             var t = System.TimeSpan.FromSeconds(GameAlwaysAlive.Instance.runningTime);
 
+            // :Timer :Text
             timerText.text = $"You're Late! {($"{t.Minutes:D2}m {t.Seconds:D2}s")}";
         }
     }
